a8a542f9e9fe98b42dba679a067252c66b8cca47
Oleksii Usatov <usatovalexey81@gmail.com>
2021-04-04 19:03:35 +0300

    Update salt

--- core/util.cpp
+++ core/util.cpp
@@ -10,7 +10,9 @@ Util::Util()
 
 void Util::getEncKeys(EncKeys *encKeys, const std::string &masterPass) {
     const EVP_CIPHER *cipher;
-    const unsigned char *salt = NULL;
+    std::string salt_s = std::string(masterPass);
+    std::reverse(salt_s.begin(), salt_s.end());
+    const unsigned char *salt = (unsigned char*) salt_s.c_str();
     cipher = EVP_get_cipherbyname("aes-256-cbc");
     EVP_BytesToKey(cipher, EVP_get_digestbyname("md5"), salt,
                (unsigned char *) masterPass.c_str(),
